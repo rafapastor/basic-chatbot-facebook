8174b6777137b381ec9c179e8f0970a8b59d434c
Rafael Pastor <rafapas22@gmail.com>
2025-02-16 20:13:38 +0100

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,31 +1,43 @@
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from flask import Flask, request, render_template
+from flask_cors import CORS
+import json
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
-model_name = "facebook/blenderbot-400M-distill"
+app = Flask(__name__)
+CORS(app)
 
-# Load model (download on first run and reference local installation for consequent runs)
+model_name = "facebook/blenderbot-90M"
 model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-
 conversation_history = []
 
-while True:
-    # Create conversation history string
-    history_string = "\n".join(conversation_history)
+@app.route('/chatbot', methods=['POST'])
+def handle_prompt():
+    data = request.get_data(as_text=True)
+    data = json.loads(data)
+    input_text = data['prompt']
 
-    # Get the input data from the user
-    input_text = input("> ")
+    # Create conversation history string
+    history = "\n".join(conversation_history)
 
     # Tokenize the input text and history
-    inputs = tokenizer.encode_plus(history_string, input_text, return_tensors="pt")
+    inputs = tokenizer.encode_plus(history, input_text, return_tensors="pt")
 
     # Generate the response from the model
-    outputs = model.generate(**inputs)
+    outputs = model.generate(**inputs, max_length= 60)  # max_length will cause the model to crash at some point as history grows
 
     # Decode the response
     response = tokenizer.decode(outputs[0], skip_special_tokens=True).strip()
-    
-    print(response)
 
     # Add interaction to conversation history
     conversation_history.append(input_text)
     conversation_history.append(response)
+
+    return response
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
